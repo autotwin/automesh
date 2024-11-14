a80219e281337a9040b2b9bedd8069f959aa7fb7
Chad Hovey <hovey@me.com>
2024-11-14 17:28:37 -0500

    starts of quadtree

---
+++ src/tree/mod.rs
@@ -0,0 +1,116 @@
+#[cfg(test)]
+pub mod test;
+
+
+#[derive(Debug)]
+pub struct Point2D {
+    pub x: f64,
+    pub y: f64,
+}
+
+
+#[derive(Debug)]
+pub struct Cell2D {
+    origin: Point2D,
+    width: f64,
+    height: f64,
+}
+
+
+impl Cell2D {
+    fn contains(&self, point: Point2D) -> bool {
+        point.x >= self.origin.x &&
+        point.x < self.origin.x + self.width &&
+        point.y >= self.origin.y &&
+        point.y < self.origin.y + self.height
+    }
+}
+
+
+#[derive(Debug)]
+pub struct QuadTree {
+    cell: Cell2D,
+    capacity: usize,
+    points: Vec<Point2D>,
+    divided: bool,
+    nw: Option<Box<QuadTree>>,  // northwest
+    ne: Option<Box<QuadTree>>,  // northeast
+    sw: Option<Box<QuadTree>>,  // southwest
+    se: Option<Box<QuadTree>>,  // southeast
+}
+
+impl QuadTree {
+    fn new(cell: Cell2D, capacity: usize) -> Self {
+        QuadTree {
+            cell,
+            capacity,
+            points: Vec::new(),
+            divided: false,
+            nw: None,
+            ne: None,
+            sw: None,
+            se: None,
+        }
+    }
+
+    fn subdivide(&mut self) {
+        let x = self.cell.origin.x;
+        let y = self.cell.origin.y;
+        let width = self.cell.width / 2.0;
+        let height = self.cell.height / 2.0;
+
+        self.nw = Some(Box::new(
+            QuadTree::new(
+                Cell2D {
+                    origin: Point2D { x, y: y + height },
+                    width,
+                    height
+                },
+                self.capacity,
+            )
+        ));
+
+        self.ne = Some(Box::new(
+            QuadTree::new(
+                Cell2D {
+                    origin: Point2D { x: x + width, y: y + height },
+                    width,
+                    height
+                },
+                self.capacity,
+            )
+        ));
+
+        self.sw = Some(Box::new(
+            QuadTree::new(
+                Cell2D {
+                    origin: Point2D { x, y },
+                    width,
+                    height
+                },
+                self.capacity,
+            )
+        ));
+
+        self.se = Some(Box::new(
+            QuadTree::new(
+                Cell2D {
+                    origin: Point2D { x: x + width, y },
+                    width,
+                    height
+                },
+                self.capacity,
+            )
+        ));
+
+    }
+}
+
+pub fn reverse(input: &str) -> String {
+    // input.chars() converts a string slice into an iterator of its
+    // characters, which is important because it handles Unicode
+    // charcters correctly.
+    // .rev() reverses the order of the characters in the iterator
+    // .collect() collects all the characters back into a String
+    input.chars().rev().collect()
+}
