cfd5717086672ad34a96db3ae3ea4aab41f660a3
Michael R. Buche <mrbuche@sandia.gov>
2025-05-19 09:06:40 -0600

    ruff fixes

--- book/smoothing/smoothing_figures.py
+++ book/smoothing/smoothing_figures.py
@@ -14,7 +14,6 @@ from typing import Final
 
 from matplotlib.colors import LightSource
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 import smoothing as sm
