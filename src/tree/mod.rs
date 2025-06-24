2c8a6c55601ee03533f069b2107dd69e215d643a
Michael R. Buche <mrbuche@sandia.gov>
2025-06-24 10:28:11 -0600

    fourth vertex template

--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -2026,6 +2026,7 @@ impl From<Octree> for HexahedralFiniteElements {
         hex::vertex_template_1::apply(&cells_nodes, &tree, &mut element_node_connectivity);
         hex::vertex_template_2::apply(&cells_nodes, &tree, &mut element_node_connectivity);
         hex::vertex_template_3::apply(&cells_nodes, &tree, &mut element_node_connectivity);
+        hex::vertex_template_4::apply(&cells_nodes, &tree, &mut element_node_connectivity);
         let fem = Self::from_data(
             vec![1; element_node_connectivity.len()],
             element_node_connectivity,
