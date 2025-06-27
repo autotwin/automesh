57521c20302a3f13322b2047bd0bc826308196b3
Michael R. Buche <mrbuche@sandia.gov>
2025-06-27 12:49:57 -0600

    edge template 4

--- src/tree/hex/edge_template_4/mod.rs
+++ src/tree/hex/edge_template_4/mod.rs
@@ -18,6 +18,90 @@ pub fn apply(
                 6,
                 3,
                 5,
+                7,
+                5,
+                2,
+                0,
+                cells_nodes,
+                cell_subcells,
+                nodes_map,
+                tree,
+                element_node_connectivity,
+                nodal_coordinates,
+            );
+            template(
+                5,
+                7,
+                5,
+                1,
+                3,
+                1,
+                6,
+                4,
+                cells_nodes,
+                cell_subcells,
+                nodes_map,
+                tree,
+                element_node_connectivity,
+                nodal_coordinates,
+            );
+            template(
+                5,
+                4,
+                0,
+                5,
+                6,
+                7,
+                0,
+                1,
+                cells_nodes,
+                cell_subcells,
+                nodes_map,
+                tree,
+                element_node_connectivity,
+                nodal_coordinates,
+            );
+            template(
+                7,
+                6,
+                5,
+                2,
+                2,
+                3,
+                4,
+                5,
+                cells_nodes,
+                cell_subcells,
+                nodes_map,
+                tree,
+                element_node_connectivity,
+                nodal_coordinates,
+            );
+            template(
+                6,
+                2,
+                3,
+                2,
+                3,
+                7,
+                0,
+                4,
+                cells_nodes,
+                cell_subcells,
+                nodes_map,
+                tree,
+                element_node_connectivity,
+                nodal_coordinates,
+            );
+            template(
+                7,
+                3,
+                2,
+                1,
+                1,
+                5,
+                2,
+                6,
                 cells_nodes,
                 cell_subcells,
                 nodes_map,
@@ -34,6 +118,10 @@ fn template(
     subcell_b: usize,
     face_m: usize,
     face_n: usize,
+    subcell_face_m_p: usize,
+    subcell_face_m_q: usize,
+    subcell_face_n_p: usize,
+    subcell_face_n_q: usize,
     cells_nodes: &[usize],
     cell_subcells: &[usize; NUM_OCTANTS],
     nodes_map: &mut NodeMap,
@@ -63,11 +151,13 @@ fn template(
                                                 if let Some((subcell_b_face_n_subcells, _)) = tree
                                                     .cell_contains_leaves(&tree[subcell_b_face_n])
                                                 {
-                                                    let lngth = *tree[subcell_a_face_m_subcells[7]]
+                                                    let lngth = *tree[subcell_a_face_m_subcells
+                                                        [subcell_face_m_p]]
                                                         .get_lngth()
                                                         as f64;
                                                     let coordinates_1 = &nodal_coordinates
-                                                        [cells_nodes[subcell_a_face_m_subcells[7]]
+                                                        [cells_nodes[subcell_a_face_m_subcells
+                                                            [subcell_face_m_p]]
                                                             - NODE_NUMBERING_OFFSET]
                                                         - face_direction(face_m) * lngth;
                                                     let node_1 = *nodes_map
@@ -78,7 +168,8 @@ fn template(
                                                         ))
                                                         .expect("nonexistent entry");
                                                     let coordinates_2 = &nodal_coordinates
-                                                        [cells_nodes[subcell_b_face_m_subcells[5]]
+                                                        [cells_nodes[subcell_b_face_m_subcells
+                                                            [subcell_face_m_q]]
                                                             - NODE_NUMBERING_OFFSET]
                                                         - face_direction(face_m) * lngth;
                                                     let node_2 = *nodes_map
@@ -89,7 +180,8 @@ fn template(
                                                         ))
                                                         .expect("nonexistent entry");
                                                     let coordinates_3 = &nodal_coordinates
-                                                        [cells_nodes[subcell_a_face_m_subcells[7]]
+                                                        [cells_nodes[subcell_a_face_m_subcells
+                                                            [subcell_face_m_p]]
                                                             - NODE_NUMBERING_OFFSET]
                                                         + face_direction(face_n) * lngth;
                                                     let node_3 = *nodes_map
@@ -100,7 +192,8 @@ fn template(
                                                         ))
                                                         .expect("nonexistent entry");
                                                     let coordinates_4 = &nodal_coordinates
-                                                        [cells_nodes[subcell_b_face_m_subcells[5]]
+                                                        [cells_nodes[subcell_b_face_m_subcells
+                                                            [subcell_face_m_q]]
                                                             - NODE_NUMBERING_OFFSET]
                                                         + face_direction(face_n) * lngth;
                                                     let node_4 = *nodes_map
@@ -111,33 +204,45 @@ fn template(
                                                         ))
                                                         .expect("nonexistent entry");
                                                     element_node_connectivity.push([
-                                                        cells_nodes[subcell_a_face_m_subcells[7]],
+                                                        cells_nodes[subcell_a_face_m_subcells
+                                                            [subcell_face_m_p]],
                                                         node_1,
                                                         node_2,
-                                                        cells_nodes[subcell_b_face_m_subcells[5]],
+                                                        cells_nodes[subcell_b_face_m_subcells
+                                                            [subcell_face_m_q]],
                                                         node_3,
-                                                        cells_nodes[subcell_a_face_n_subcells[2]],
-                                                        cells_nodes[subcell_b_face_n_subcells[0]],
+                                                        cells_nodes[subcell_a_face_n_subcells
+                                                            [subcell_face_n_p]],
+                                                        cells_nodes[subcell_b_face_n_subcells
+                                                            [subcell_face_n_q]],
                                                         node_4,
                                                     ]);
                                                     element_node_connectivity.push([
-                                                        cells_nodes[subcell_b_face_m_subcells[5]],
+                                                        cells_nodes[subcell_b_face_m_subcells
+                                                            [subcell_face_m_q]],
                                                         node_2,
                                                         cells_nodes[cell_subcells[subcell_b]],
-                                                        cells_nodes[subcell_b_face_m_subcells[7]],
+                                                        cells_nodes[subcell_b_face_m_subcells
+                                                            [subcell_face_m_p]],
                                                         node_4,
-                                                        cells_nodes[subcell_b_face_n_subcells[0]],
-                                                        cells_nodes[subcell_b_face_n_subcells[2]],
+                                                        cells_nodes[subcell_b_face_n_subcells
+                                                            [subcell_face_n_q]],
+                                                        cells_nodes[subcell_b_face_n_subcells
+                                                            [subcell_face_n_p]],
                                                         cells_nodes[diagonal_b],
                                                     ]);
                                                     element_node_connectivity.push([
-                                                        cells_nodes[subcell_a_face_m_subcells[5]],
+                                                        cells_nodes[subcell_a_face_m_subcells
+                                                            [subcell_face_m_q]],
                                                         cells_nodes[cell_subcells[subcell_a]],
                                                         node_1,
-                                                        cells_nodes[subcell_a_face_m_subcells[7]],
+                                                        cells_nodes[subcell_a_face_m_subcells
+                                                            [subcell_face_m_p]],
                                                         cells_nodes[diagonal_a],
-                                                        cells_nodes[subcell_a_face_n_subcells[0]],
-                                                        cells_nodes[subcell_a_face_n_subcells[2]],
+                                                        cells_nodes[subcell_a_face_n_subcells
+                                                            [subcell_face_n_q]],
+                                                        cells_nodes[subcell_a_face_n_subcells
+                                                            [subcell_face_n_p]],
                                                         node_3,
                                                     ]);
                                                 }
