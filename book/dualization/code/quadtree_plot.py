0667690b94381bb2eb9997285c387b2b626d0fb8
Chad Hovey <hovey@me.com>
2025-06-30 22:04:58 -0400

    pause for the night

--- book/dualization/code/quadtree_plot.py
+++ book/dualization/code/quadtree_plot.py
@@ -2,12 +2,15 @@
 
 from pathlib import Path
 from typing import Final
-from typing import NamedTuple
+# from typing import NamedTuple
 
 
 import matplotlib.pyplot as plt
 from matplotlib import patches
 
+from colorschemes import QuadColors
+
+# from colorschemes import Color, level_to_color
 
 # class Seed(NamedTuple):
 #     """The (x, y) point used to trigger refinement."""
@@ -15,25 +18,25 @@ from matplotlib import patches
 #     y: float
 
 
-class ColorScheme(NamedTuple):
-    """A color scheme for the quadtree.
-    The plasma colorscheme goes from dark purple to bright yellow.  The plasma_r
-    reverses the plasma colorscheme, thus it goes from bright yellow to dark purple.
-    """
-
-    edgecolor: str
-    facecolor: str
-    zorder: int
-    alpha: float = 0.3
-
-
-def level_to_colorscheme(level: int) -> ColorScheme:
-    """Returns a color scheme based on the level of the quadtree."""
-    colors = ["dimgray", "lightgray", "silver", "gainsboro", "whitesmoke", "white"]
-    edgecolor = "black"
-    facecolor = colors[level % len(colors)]
-    zorder = 2 + level
-    return ColorScheme(edgecolor, facecolor, zorder)
+# class ColorScheme(NamedTuple):
+#     """A color scheme for the quadtree.
+#     The plasma colorscheme goes from dark purple to bright yellow.  The plasma_r
+#     reverses the plasma colorscheme, thus it goes from bright yellow to dark purple.
+#     """
+#
+#     edgecolor: str
+#     facecolor: str
+#     zorder: int
+#     alpha: float = 0.3
+#
+#
+# def level_to_colorscheme(level: int) -> ColorScheme:
+#     """Returns a color scheme based on the level of the quadtree."""
+#     colors = ["dimgray", "lightgray", "silver", "gainsboro", "whitesmoke", "white"]
+#     edgecolor = "black"
+#     facecolor = colors[level % len(colors)]
+#     zorder = 2 + level
+#     return ColorScheme(edgecolor, facecolor, zorder)
 
 
 class QuadTree:
@@ -46,17 +49,22 @@ class QuadTree:
         self.boundary = (x, y, width, height)
         self.level = level
         self.max_level = max_level
+        self.has_children = False
         self.children = []
         self.subdivide()
 
     def subdivide(self):
         """Divides the parent quad into four quad children."""
-        if self.level < self.max_level:
+        if self.level <= self.max_level:
+            print(
+                f"Subdividing quad at level {self.level} with boundary {self.boundary}"
+            )
             x, y, width, height = self.boundary
             half_width = width / 2.0
             half_height = height / 2.0
 
             # Create four children
+            self.has_children = True  # overwrite
             self.children.append(
                 QuadTree(
                     x,
@@ -98,74 +106,79 @@ class QuadTree:
                 )
             )  # Bottom-right
 
-    def draw(self, ax, edgecolor="black", color="dimgray"):
+    def draw(self, ax, quadcolors: QuadColors):
         """Draw the quadtree."""
         x, y, width, height = self.boundary
         # Draw the boundary rectangle
+        print(
+            f"Drawing level {self.level} quad at ({x}, {y}) with width {width} and height {height}"
+        )
         rect = patches.Rectangle(
             (x, y),
             width,
             height,
             # linewidth=1,
             linestyle="solid",
-            edgecolor="black",
-            # facecolor="dimgray",
-            facecolor=color,
-            alpha=0.3,
+            edgecolor=quadcolors.edgecolor,
+            facecolor=quadcolors.facecolors[self.level],
+            alpha=quadcolors.alpha,
             zorder=2,
         )
         ax.add_patch(rect)
 
         # Draw children
-        for child in self.children:
-            child.draw(ax)
+        # if self.has_children:
+        #     print(f"Quad at level {self.level} has children, drawing them.")
+        #     for child in self.children:
+        #         child.draw(ax, quadcolors)
 
 
 def main():
     # User input begin
-    N_LEVELS: Final[int] = 0
+    MAX_LEVEL: Final[int] = 2
     SAVE: Final[bool] = True
     SHOW: Final[bool] = True
     EXT: Final[str] = ".png"  # ".pdf" | ".png" | ".svg"
     DPI: Final[int] = 300
 
-    XMIN = 1
-    XMAX = 3
-    YMIN = -1
-    YMAX = 1
+    XMIN = -2  # 1
+    XMAX = 2  # 3
+    YMIN = -2  # -1
+    YMAX = 2  # 1
     WIDTH = XMAX - XMIN
     HEIGHT = YMAX - YMIN
     # User input end
 
-    colors = (
-        "tab:blue",
-        "tab:orange",
-        "tab:green",
-        "tab:red",
-        "tab:purple",
-        "tab:brown",
-        "tab:pink",
-        "tab:gray",
-        "tab:olive",
-    )
-
     # Create a figure and axis
-    figwidth, figheight = 6, 6  # inches, inches
-    fig, ax0 = plt.subplots(figsize=(figwidth, figheight))
+    width, height = 6, 6  # inches, inches
+    fig, ax = plt.subplots(figsize=(width, height))
 
     # Create the quadtree with a boundary of (-12, -12, 24, 24)
-    quadtree = QuadTree(XMIN, YMIN, WIDTH, HEIGHT, level=0, max_level=N_LEVELS)
+    quadtree = QuadTree(XMIN, YMIN, WIDTH, HEIGHT, level=0, max_level=MAX_LEVEL)
 
+    quadcolors = QuadColors(
+        n_levels=MAX_LEVEL + 1,  # +1 because we start at level 0
+        edgecolor="black",
+        alpha=0.3,
+        plasma=True,
+        reversed=False,
+    )
+    print(f"quadcolors.facecolors: {quadcolors.facecolors}")
+    breakpoint()
     # Draw the quadtree
-    quadtree.draw(ax0)
+    quadtree.draw(ax, quadcolors)
 
     # Set limits and aspect
     MARGIN = 0.1 * (XMAX - XMIN)
-    ax0.set_xlim(XMIN - MARGIN, XMAX + MARGIN)
-    ax0.set_ylim(YMIN - MARGIN, YMAX + MARGIN)
-    ax0.set_aspect("equal")
-    GRAMMAR_LEVELS = f"{N_LEVELS} Level" if N_LEVELS == 1 else f"{N_LEVELS} Levels"
-    ax0.set_title(f"Quadtree with {GRAMMAR_LEVELS} of Refinement")
+    ax.set_xlim(XMIN - MARGIN, XMAX + MARGIN)
+    ax.set_ylim(YMIN - MARGIN, YMAX + MARGIN)
+    ax.set_aspect("equal")
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_xticks([])
+    ax.set_yticks([])
+    GRAMMAR_LEVELS = f"{MAX_LEVEL} Level" if MAX_LEVEL == 1 else f"{MAX_LEVEL} Levels"
+    ax.set_title(f"Quadtree with {GRAMMAR_LEVELS} of Refinement")
     plt.grid()
     plt.show()
 
@@ -174,7 +187,7 @@ def main():
 
     if SAVE:
         parent = Path(__file__).parent
-        stem = Path(__file__).stem + "_level_" + str(N_LEVELS)
+        stem = Path(__file__).stem + "_level_" + str(MAX_LEVEL)
         fn = parent.joinpath(stem + EXT)
         # plt.savefig(fn, dpi=DPI, bbox_inches='tight')
         fig.savefig(fn, dpi=DPI)
