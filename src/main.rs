971aac0bd67efdd0070e9042d615aed0a4e84555
Chad Hovey <hovey@me.com>
2025-12-17 17:12:21 -0500

    crates.io instead of 000000

--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,7 @@ macro_rules! about {
             format!("v{} {} {}", env!("CARGO_PKG_VERSION"), OS, ARCH,),
             format!(
                 "build {} {}",
-                option_env!("GIT_COMMIT_HASH").unwrap_or("0000000"),
+                option_env!("GIT_COMMIT_HASH").unwrap_or("crates.io"),
                 env!("BUILD_TIME"),
             ),
             env!("CARGO_PKG_AUTHORS").split(':').next().unwrap_or(""),
